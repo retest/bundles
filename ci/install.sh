eddfdea6d8a2c651a480fd07222349f4f640bf45
Johannes Dillmann <johannes.dillmann@retest.de>
2019-04-03 17:11:31 +0200

    fix

--- ci/install.sh
+++ ci/install.sh
@@ -11,7 +11,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     OS="osx"
     URL="https://downloads.rclone.org/${RCLONE_VERSION}/rclone-${RCLONE_VERSION}-${OS}-amd64.zip"
-    wget ${URL}
+    wget --quiet ${URL}
     # echo ${RCLONE_HASH} "rclone-${RCLONE_VERSION}-${OS}-amd64.zip" | md5sum --check -
     unzip -j -q "rclone-${RCLONE_VERSION}-${OS}-amd64.zip" "rclone-${RCLONE_VERSION}-${OS}-amd64/rclone" -d ${HOME}/bin/
 fi
