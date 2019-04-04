1fba2e6720f9695a2556c20f64097cf4a3ee4236
Johannes Dillmann <johannes.dillmann@retest.de>
2019-04-04 09:52:35 +0200

    Add Linux builds

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
+BUCKET='DO:retest'
+SOURCE_PATH=${TRAVIS_BUILD_DIR}/packages
+TARGET_PATH='bundles'
+
 ## Linux stuff
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Deploying Linux bundles ..."
+
+    # rclone copy ${SOURCE_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}/${TARGET_PATH}
 fi
 
 ## OSX stuff
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo "Deploying Mac bundles ..."
+
+    rclone copy ${SOURCE_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}/${TARGET_PATH}
 fi
 
 ## Windows stuff
--- ci/install.sh
+++ ci/install.sh
@@ -3,6 +3,11 @@
 ## Linux stuff
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Installing Linux dependencies ..."
+
+    OS="linux"
+    URL="https://downloads.rclone.org/${RCLONE_VERSION}/rclone-${RCLONE_VERSION}-${OS}-amd64.zip"
+    wget --quiet ${URL}
+    unzip -j -q "rclone-${RCLONE_VERSION}-${OS}-amd64.zip" "rclone-${RCLONE_VERSION}-${OS}-amd64/rclone" -d ${HOME}/bin/
 fi
 
 ## OSX stuff
@@ -12,7 +17,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     OS="osx"
     URL="https://downloads.rclone.org/${RCLONE_VERSION}/rclone-${RCLONE_VERSION}-${OS}-amd64.zip"
     wget --quiet ${URL}
-    # echo ${RCLONE_HASH} "rclone-${RCLONE_VERSION}-${OS}-amd64.zip" | md5sum --check -
     unzip -j -q "rclone-${RCLONE_VERSION}-${OS}-amd64.zip" "rclone-${RCLONE_VERSION}-${OS}-amd64/rclone" -d ${HOME}/bin/
 fi
 
