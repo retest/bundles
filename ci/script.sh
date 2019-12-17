f01a9c829263b32c30377209ac788e393faac7b5
Marius Kemmling <marius.kemmling@retest.de>
2019-12-17 16:36:48 +0100

    Cleanup test setup

--- ci/script.sh
+++ ci/script.sh
@@ -16,12 +16,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     curl --location ${ICON_URL} --output ${TRAVIS_BUILD_DIR}/${ICON}
 
     echo "Building Linux bundles ..."
-
     ${TRAVIS_BUILD_DIR}/jdk-14/bin/jpackage --type deb --verbose -d ${TRAVIS_BUILD_DIR}/packages -i ${TRAVIS_BUILD_DIR}/review -n review --main-class de.retest.gui.ReTestGui --main-jar review.jar --icon ${TRAVIS_BUILD_DIR}/${ICON} --runtime-image ${TRAVIS_BUILD_DIR}/runtime --vendor "ReTest GmbH" --copyright "ReTest GmbH" --app-version "${VERSION}" --linux-package-name "review"
-
-    ls ${TRAVIS_BUILD_DIR}/packages
-
-    rclone copy ${TRAVIS_BUILD_DIR}/packages/review_1.0.0-1_amd64.deb ${BUCKET}/releases/review/bundles/${TRAVIS_BUILD_NUMBER}
 fi
 
 ## OSX stuff
@@ -36,17 +31,13 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     curl --location ${ICON_URL} --output ${TRAVIS_BUILD_DIR}/${ICON}
 
     echo "Building macOS bundles ..."
-
     ${TRAVIS_BUILD_DIR}/jdk-14.jdk/Contents/Home/bin/jpackage --type app-image --verbose -d ${TRAVIS_BUILD_DIR}/packages -i ${TRAVIS_BUILD_DIR}/review -n review --main-class de.retest.gui.ReTestGui --main-jar review.jar --icon ${TRAVIS_BUILD_DIR}/favicon.icns --runtime-image ${TRAVIS_BUILD_DIR}/runtime --vendor "ReTest GmbH" --copyright "ReTest GmbH" --app-version "${VERSION}" --mac-package-name "review"
 
+    echo "Adjust Info.plist to open review with browser link ..."
     /usr/libexec/PlistBuddy -c "add :CFBundleURLTypes array" -c "add :CFBundleURLTypes:0 dict" -c "add :CFBundleURLTypes:0:CFBundleURLSchemes array" -c "add :CFBundleURLTypes:0:CFBundleURLSchemes:0 string review" -c "add :CFBundleURLTypes:0:CFBundleURLName string " ${TRAVIS_BUILD_DIR}/packages/review.app/Contents/Info.plist
 
     cd ${TRAVIS_BUILD_DIR}/packages
     zip -r -q review.zip review.app
-
-    rclone copy ${TRAVIS_BUILD_DIR}/packages/review.zip ${BUCKET}/releases/review/bundles/${TRAVIS_BUILD_NUMBER}
-
-    find ${TRAVIS_BUILD_DIR}/packages
 fi
 
 ## Windows stuff
